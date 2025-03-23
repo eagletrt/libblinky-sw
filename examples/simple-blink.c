eeaa5360cd76bea7da355c3207763817903d449a
Bridiro <ale.bridi15@gmail.com>
2025-03-23 18:50:38 +0100

    feat: more complete example #1

--- examples/simple-blink.c
+++ examples/simple-blink.c
@@ -1,20 +1,29 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <unistd.h> // For usleep
+
 #include "blinky-api.h"
 
-#define PATTERN_SIZE 2
-
-uint16_t pattern[PATTERN_SIZE] = {
-    1000,
-    1000
-};
-
 int main() {
-    Blinky b;
-    blinky_init(&b, pattern, PATTERN_SIZE, true, BLINKY_LOW);
+    Blinky blinker;
+    uint16_t pattern[] = {100, 100}; // Example pattern: 100 ms on, 100 ms off
+    uint8_t size = 2;
 
-    // Do your things
-    BlinkyState state = blinky_routine(&b, time_in_ms); // call this sometimes to get the state to set
+    blinky_init(&blinker, pattern, size, true, BLINKY_LOW);
+    blinky_enable(&blinker, true);
 
-    led_set(state);
+    uint32_t start_time = 0;
+    uint32_t elapsed_time = 0;
 
+    while (elapsed_time < 5000) { // Run for 5 seconds
+        BlinkyState state = blinky_routine(&blinker, elapsed_time);
+        printf("Time: %d ms, LED State: %s\n", elapsed_time, state == BLINKY_HIGH ? "ON" : "OFF");
+
+        usleep(200 * 1000); // Sleep for 200 ms
+        elapsed_time += 200;
+    }
+
+    blinky_enable(&blinker, false); // Disable after 5 seconds
     return 0;
 }
